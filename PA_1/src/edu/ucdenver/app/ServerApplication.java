e20ec12885dde95dfc33d7e1cbfcaaa093d1aadb
lexselby <alexa.selby01@gmail.com>
2022-11-15 19:08:26 -0700

    made a few minor changes to the server application menu

--- PA_1/src/edu/ucdenver/app/ServerApplication.java
+++ PA_1/src/edu/ucdenver/app/ServerApplication.java
@@ -12,76 +12,80 @@ public class ServerApplication {
         //init Server object
         Server server = new Server();
         Tournament tournament = new Tournament(null, null, null);
+        Boolean stillRunning = true;
 
         //TODO: create option menu to display in console via text
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Select option\n[0] START SERVER\n[1] STOP SERVER\n[2] LOAD FROM FILE\n[3] SAVE TO FILE");
-        String choice = sc.nextLine();
+        while (stillRunning) {
+            // create menu
+            try {
 
-        // inform server of app type (ADMIN or USER)
-        Scanner sc1 = new Scanner(System.in);
-        System.out.println("Select client type:\n[A] ADMIN\n[U] USER\n");
-        String type = sc1.nextLine();
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Select option\n[0] START SERVER\n[1] STOP SERVER\n[2] LOAD FROM FILE\n[3] SAVE TO FILE");
+                String choice = sc.nextLine();
 
-        // create menu
-        try {
-            switch (choice) {
+                // inform server of app type (ADMIN or USER)
+                Scanner sc1 = new Scanner(System.in);
+                System.out.println("Select client type:\n[A] ADMIN\n[U] USER\n");
+                String type = sc1.nextLine();
 
-                //--------------------------------------------------
-                //                [0] start server
-                //--------------------------------------------------
-                case "0":
-                    try {
-                        server = new Server(9888, 10, type, tournament);
-                        server.run();
+                switch (choice) {
 
-                    } catch (Exception e) {
-                        System.err.print(e);
-                    }
+                    //--------------------------------------------------
+                    //                [0] start server
+                    //--------------------------------------------------
+                    case "0":
+                        try {
+                            server = new Server(9888, 10, type, tournament);
+                            server.run();
 
-
-                //--------------------------------------------------
-                //                [1] stop server
-                //--------------------------------------------------
-                case "1":
-                    try {
-                        server.stop();
-                    } catch (Exception e) {
-                        System.err.print(e);
-                    }
+                        } catch (Exception e) {
+                            System.err.print(e);
+                        }
+                        break;
 
 
-                //--------------------------------------------------
-                //             [2] load previous state
-                //--------------------------------------------------
-                case "2":
-                    try {
-                        Tournament.loadFromFile("tournament.ser");
-                        server = new Server(9888, 10, type, tournament);
-                        System.out.println("Loaded previous state.");
-                    } catch (Exception e) {
-                        System.err.print(e);
-                    }
+                        //--------------------------------------------------
+                        //                [1] stop server
+                        //--------------------------------------------------
+                    case "1":
+                        try {
+                            server.stop();
+                            stillRunning = false;
+                        } catch (Exception e) {
+                            System.err.print(e);
+                        }
+                        break;
 
-                //--------------------------------------------------
-                //            [3] save current state
-                //--------------------------------------------------
-                case "3":
-                    try {
-                        tournament.saveToFile("tournament.ser");
-                        System.out.println("Saved current state.");
-                    }
-                    catch (Exception e){
-                        System.err.print(e);
-                    }
+
+                        //--------------------------------------------------
+                        //             [2] load previous state
+                        //--------------------------------------------------
+                    case "2":
+                        try {
+                            Tournament.loadFromFile("tournament.ser");
+                            server = new Server(9888, 10, type, tournament);
+                            System.out.println("Loaded previous state.\n");
+                        } catch (Exception e) {
+                            System.err.print(e);
+                        }
+                        break;
+
+                        //--------------------------------------------------
+                        //            [3] save current state
+                        //--------------------------------------------------
+                    case "3":
+                        try {
+                            tournament.saveToFile("tournament.ser");
+                            System.out.println("Saved current state.\n");
+                        } catch (Exception e) {
+                            System.err.print(e);
+                        }
+                        break;
+                }
+            } catch (IllegalArgumentException iae) {
+                System.out.println("ERROR| " + iae.getMessage());
             }
         }
-        catch (IllegalArgumentException iae) {
-                System.out.println("ERROR| " + iae.getMessage());
-        }
-
-
     }
-
 }
