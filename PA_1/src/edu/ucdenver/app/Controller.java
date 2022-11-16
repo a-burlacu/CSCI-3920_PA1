57c01761cc7b4b408dbfcc6cad16838dc554ca43
lexselby <alexa.selby01@gmail.com>
2022-11-16 01:55:20 -0700

    worked on client admin app gui

--- PA_1/src/edu/ucdenver/app/Controller.java
+++ PA_1/src/edu/ucdenver/app/Controller.java
@@ -12,25 +12,67 @@ import java.util.List;
 
 public class Controller {
     public TextField txtNewTournamentName;
+
     public Button btnNewTournamentSubmit;
     public DatePicker dtpNewTournamentStartDate;
     public DatePicker dtpNewTournamentEndDate;
     public TextField txtAddCountry;
+
     public Button btnAddCountrySubmit;
     public TextField txtAddTeamName;
-    public Button btnAddTeam;
+    public Button btnAddTeamSubmit;
     public ComboBox selAddTeamCountry;
+    public TextField txtAddRefereeName;
+    public Button btnAddRefereeSubmit;
+    public ComboBox selAddRefereeCountry;
+    public Button btnAddPlayerToSquadSubmit;
+    public TextField txtAddPlayerToSquadName;
+    public TextField txtAddPlayerToSquadAge;
+    public TextField txtAddPlayerToSquadHeight;
+    public TextField txtAddPlayerToSquadWeight;
+    public ComboBox selAddPlayerToSquadTeam;
+    public Button btnAddMatchSubmit;
+    public ComboBox selAddMatchTeamA;
+    public ComboBox selAddMatchTeamB;
+    public DatePicker dtpAddMatchDate;
+    public Button btnAddRefereeToMatchSubmit;
+    public ComboBox selAddRefereeToMatchDate;
+    public ComboBox selAddRefereeName;
+    public Button btnAddPlayerToLineUpSubmit;
+    public ComboBox selAddPlayerToLineupMatch;
+    public ComboBox selAddPlayerToLineUpTeam;
+    public ComboBox selAddPlayerToLineUpPlayer;
+    public Button btnSetMatchScoreSubmit;
+    public TextField txtSetMatchScore1;
+    public TextField txtSetMatchScore2;
+    public ComboBox selSetMatchScoreDate;
     Client client;
 
     private List<String> countries;
-
-
+    private List<String> teams;
+    private List<String> referees;
+    private List<LocalDate> matches;
+    private List<String> players;
 
     public Controller() {
         client = new Client();
         client.connect();
         this.countries = new ArrayList<>();
+        this.teams = new ArrayList<>();
+        this.referees = new ArrayList<>();
+        this.matches = new ArrayList<>();
+        this.players = new ArrayList<>();
         this.selAddTeamCountry = new ComboBox();
+        this.selAddRefereeCountry = new ComboBox();
+        this.selAddPlayerToSquadTeam = new ComboBox();
+        this.selAddMatchTeamA = new ComboBox();
+        this.selAddMatchTeamB = new ComboBox();
+        this.selAddRefereeToMatchDate = new ComboBox();
+        this.selAddRefereeName = new ComboBox<>();
+        this.selAddPlayerToLineupMatch = new ComboBox();
+        this.selAddPlayerToLineUpTeam = new ComboBox<>();
+        this.selAddPlayerToLineUpPlayer = new ComboBox();
+        this.selSetMatchScoreDate = new ComboBox<>();
     }
 
     private String sendCommand(String cmd) {
@@ -73,6 +115,7 @@ public class Controller {
 
     public void addCountry(ActionEvent actionEvent) {
         String countryNameText = this.txtAddCountry.getText();
+        countries.add(countryNameText);
         Alert alert;
 
         String cmd = String.format("%s|%s","4",countryNameText);
@@ -91,13 +134,160 @@ public class Controller {
         }
     }
 
-    public void addTeam(ActionEvent actionEvent) {
-        selAddTeamCountry.setItems(FXCollections.observableArrayList(countries));
+    public void AddTeam(ActionEvent actionEvent) {
+        this.selAddTeamCountry.setItems(FXCollections.observableArrayList(this.countries));
         String countryNameSelection = this.selAddTeamCountry.getValue().toString();
         String teamNameText = this.txtAddTeamName.getText();
+        this.teams.add(teamNameText);
         Alert alert;
 
         String cmd = String.format("%s|%s|%s","5", countryNameSelection, teamNameText);
+        String response = sendCommand(cmd);
+        String[] respArgs = response.split("\\|");
+
+        switch (respArgs[0]){
+            case "OK":
+                alert = new Alert(Alert.AlertType.CONFIRMATION, "Server Response:" + response, ButtonType.OK);
+                alert.show();
+                break;
+            case "ERR":
+                alert = new Alert(Alert.AlertType.ERROR, respArgs[1], ButtonType.OK);
+                alert.show();
+                break;
+        }
+    }
+
+    public void AddReferee(ActionEvent actionEvent) {
+        this.selAddRefereeCountry.setItems(FXCollections.observableArrayList(this.countries));
+        String countryNameSelection = this.selAddRefereeCountry.getValue().toString();
+        String refereeNameText = this.txtAddRefereeName.getText();
+        this.referees.add(refereeNameText);
+        Alert alert;
+
+        String cmd = String.format("%s|%s|%s","6", countryNameSelection, refereeNameText);
+        String response = sendCommand(cmd);
+        String[] respArgs = response.split("\\|");
+
+        switch (respArgs[0]){
+            case "OK":
+                alert = new Alert(Alert.AlertType.CONFIRMATION, "Server Response:" + response, ButtonType.OK);
+                alert.show();
+                break;
+            case "ERR":
+                alert = new Alert(Alert.AlertType.ERROR, respArgs[1], ButtonType.OK);
+                alert.show();
+                break;
+        }
+    }
+
+    public void addPlayerToSquad(ActionEvent actionEvent) {
+        this.selAddPlayerToSquadTeam.setItems(FXCollections.observableArrayList(this.teams));
+        String playerTeamSelection = this.selAddPlayerToSquadTeam.getValue().toString();
+        String playerNameText = this.txtAddPlayerToSquadName.getText();
+        String playerAgeText = this.txtAddPlayerToSquadAge.getText();
+        String playerHeightText = this.txtAddPlayerToSquadHeight.getText();
+        String playerWeightText = this.txtAddPlayerToSquadWeight.getText();
+        this.players.add(playerNameText);
+        Alert alert;
+
+        String cmd = String.format("%s|%s|%s|%s|%s|%s","7", playerTeamSelection, playerNameText, playerAgeText,
+                playerHeightText, playerWeightText);
+        String response = sendCommand(cmd);
+        String[] respArgs = response.split("\\|");
+
+        switch (respArgs[0]){
+            case "OK":
+                alert = new Alert(Alert.AlertType.CONFIRMATION, "Server Response:" + response, ButtonType.OK);
+                alert.show();
+                break;
+            case "ERR":
+                alert = new Alert(Alert.AlertType.ERROR, respArgs[1], ButtonType.OK);
+                alert.show();
+                break;
+        }
+    }
+
+    public void addMatch(ActionEvent actionEvent) {
+        this.selAddMatchTeamA.setItems(FXCollections.observableArrayList(this.teams));
+        this.selAddMatchTeamB.setItems(FXCollections.observableArrayList(this.teams));
+        LocalDate dateOfMatch = dtpAddMatchDate.getValue();
+        String teamASelection = selAddMatchTeamA.getValue().toString();
+        String teamBSelection = selAddMatchTeamB.getValue().toString();
+        this.matches.add(dateOfMatch);
+        Alert alert;
+
+        String cmd = String.format("%s|%s|%s|%s","8", dateOfMatch.toString(), teamASelection, teamBSelection);
+        String response = sendCommand(cmd);
+        String[] respArgs = response.split("\\|");
+
+        switch (respArgs[0]){
+            case "OK":
+                alert = new Alert(Alert.AlertType.CONFIRMATION, "Server Response:" + response, ButtonType.OK);
+                alert.show();
+                break;
+            case "ERR":
+                alert = new Alert(Alert.AlertType.ERROR, respArgs[1], ButtonType.OK);
+                alert.show();
+                break;
+        }
+    }
+
+    public void addReferee(ActionEvent actionEvent) {
+        this.selAddRefereeToMatchDate.setItems(FXCollections.observableArrayList(this.matches));
+        this.selAddRefereeName.setItems(FXCollections.observableArrayList(this.referees));
+        String dateOfMatch = selAddRefereeToMatchDate.getValue().toString();
+        String refereeName = selAddRefereeName.getValue().toString();
+        Alert alert;
+
+        String cmd = String.format("%s|%s|%s","9", dateOfMatch, refereeName);
+        String response = sendCommand(cmd);
+        String[] respArgs = response.split("\\|");
+
+        switch (respArgs[0]){
+            case "OK":
+                alert = new Alert(Alert.AlertType.CONFIRMATION, "Server Response:" + response, ButtonType.OK);
+                alert.show();
+                break;
+            case "ERR":
+                alert = new Alert(Alert.AlertType.ERROR, respArgs[1], ButtonType.OK);
+                alert.show();
+                break;
+        }
+    }
+
+    public void addPlayerToLineUp(ActionEvent actionEvent) {
+        this.selAddPlayerToLineupMatch.setItems(FXCollections.observableArrayList(this.matches));
+        this.selAddPlayerToLineUpTeam.setItems(FXCollections.observableArrayList(this.teams));
+        this.selAddPlayerToLineUpPlayer.setItems(FXCollections.observableArrayList(this.players));
+        String matchDate = selAddPlayerToLineupMatch.getValue().toString();
+        String teamName = selAddPlayerToLineUpTeam.getValue().toString();
+        String playerName = selAddPlayerToLineUpPlayer.getValue().toString();
+        Alert alert;
+
+        String cmd = String.format("%s|%s|%s|%s","10", matchDate, teamName, playerName);
+        String response = sendCommand(cmd);
+        String[] respArgs = response.split("\\|");
+
+        switch (respArgs[0]){
+            case "OK":
+                alert = new Alert(Alert.AlertType.CONFIRMATION, "Server Response:" + response, ButtonType.OK);
+                alert.show();
+                break;
+            case "ERR":
+                alert = new Alert(Alert.AlertType.ERROR, respArgs[1], ButtonType.OK);
+                alert.show();
+                break;
+        }
+    }
+
+    public void setMatchScore(ActionEvent actionEvent) {
+        this.selSetMatchScoreDate.setItems(FXCollections.observableArrayList(this.matches));
+        String matchDate = selSetMatchScoreDate.getValue().toString();
+        String score1 = txtSetMatchScore1.getText();
+        String score2 = txtSetMatchScore2.getText();
+        Alert alert;
+
+        String cmd = String.format("%s|%s|%s|%s","11", matchDate, score1, score2);
         String response = sendCommand(cmd);
         String[] respArgs = response.split("\\|");
 
