936d363bb11d89398415198fbb9ba6f7c13acf86
lexselby <alexa.selby01@gmail.com>
2022-11-15 21:48:28 -0700

    worked on client user app gui

--- PA_1/src/edu/ucdenver/app/Controller.java
+++ PA_1/src/edu/ucdenver/app/Controller.java
@@ -7,50 +7,10 @@ import javafx.scene.control.*;
 import java.io.IOException;
 
 public class Controller {
-
-    public TextArea txtOutput;
-    public TextField txtTestCmd;
-    public Button btnTestSendCmd;
-
     Client client;
 
-    public Controller(){
+    public Controller() {
         client = new Client();
         client.connect();
-    }
-
-    public void testSendCmd(ActionEvent actionEvent) {
-        String cmd = txtTestCmd.getText();
-        Alert alert;
-
-        if(client.isConnected()) {
-            try {
-                String response = client.sendRequest(cmd);
-                String[] respArgs = response.split("\\|");
-
-                switch (respArgs[0]){
-                    case "OK":
-                        alert = new Alert(Alert.AlertType.CONFIRMATION, "Server Response:" + response, ButtonType.OK);
-                        alert.show();
-                        break;
-                    case "ERR":
-                        alert = new Alert(Alert.AlertType.ERROR, respArgs[1], ButtonType.OK);
-                        alert.show();
-                        break;
-                }
-                txtOutput.clear();
-                for (String s: respArgs){
-                    txtOutput.appendText(s);
-                    txtOutput.appendText(System.getProperty("line.separator"));
-                }
-            } catch (IOException e) {
-                alert = new Alert(Alert.AlertType.ERROR, "Exception:" + e.getMessage(), ButtonType.OK);
-                alert.show();
-            }
-        }
-        else {
-            alert = new Alert(Alert.AlertType.ERROR, "Client is not connected.", ButtonType.OK);
-            alert.show();
-        }
     }
 }
